a5354a9f4ebc0c82639ac934c4786c3258e927ad
FruHafizd <muhammadhafidz12333@gmail.com>
2025-12-22 20:37:11 +0800

    Create Model and migration Transctions and create function submit

--- app/Livewire/Transaksi/ModalTambah.php
+++ app/Livewire/Transaksi/ModalTambah.php
@@ -2,10 +2,43 @@
 
 namespace App\Livewire\Transaksi;
 
+use App\Models\Trasction;
 use Livewire\Component;
 
 class ModalTambah extends Component
 {
+    public $date;
+    public $amount_money;
+    public $type_transaction;
+    public $payment_method;
+    public $information;
+
+    protected $rules = [
+        'date' => 'required|date',
+        'amount_money' => 'required',
+        'type_transaction' => 'required',
+        'payment_method' => 'required',
+        'information' => 'required',
+    ];
+
+    public function submit()
+    {   
+        $this->validate();
+        try {
+            Trasction::create([
+                'date' => $this->date,
+                'amount_money' => $this->amount_money,
+                'type_transaction' => $this->type_transaction,
+                'payment_method' => $this->payment_method,
+                'information' => $this->information
+            ]);
+
+            return redirect()->to("/home");
+        } catch (\Exception $ex) {
+            
+        }
+    }
+
     public function render()
     {
         return view('livewire.transaksi.modal-tambah');
--- resources/views/livewire/transaksi/modal-tambah.blade.php
+++ resources/views/livewire/transaksi/modal-tambah.blade.php
@@ -15,60 +15,89 @@
         </div>
 
         <!-- FORM -->
-        <form class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <form wire:submit.prevent="submit" class="grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div>
                 <label class="text-sm">Tanggal</label>
-                <input type="date" class="w-full border rounded-lg p-2">
+                <input type="date" wire:model="date" class="w-full border rounded-lg p-2">
+                @error('date')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                @enderror
             </div>
 
             <div>
                 <label class="text-sm">Jumlah Uang</label>
-                <input type="number" class="w-full border rounded-lg p-2">
+                <input type="number" wire:model="amount_money" class="w-full border rounded-lg p-2">
+                @error('amount_money')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                @enderror
             </div>
 
             <div>
                 <label class="text-sm">Jenis Transaksi</label>
-                <select class="w-full border rounded-lg p-2">
-                    <option>Pemasukan</option>
-                    <option>Pengeluaran</option>
+                <select class="w-full border rounded-lg p-2" wire:model="type_transaction">
+                    <option value="">-- Pilih Metode --</option>
+                    <option value="Income">Pemasukan</option>
+                    <option value="Expenditure">Pengeluaran</option>
                 </select>
+                @error('type_transaction')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                @enderror
             </div>
 
             <div>
                 <label class="text-sm">Metode Pembayaran</label>
-                <select class="w-full border rounded-lg p-2">
-                    <option>Tunai</option>
-                    <option>Debit</option>
-                    <option>E-Wallet</option>
+                <select class="w-full border rounded-lg p-2" wire:model="payment_method">
+                    <option value="">-- Pilih Metode --</option>
+                    <option value="Cash">Tunai</option>
+                    <option value="Debit">Debit</option>
+                    <option value="E-wallet">E-Wallet</option>
                 </select>
+                @error('payment_method')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                @enderror
             </div>
 
-            <div>
+            {{-- <div>
                 <label class="text-sm">Kategori</label>
                 <input type="text" class="w-full border rounded-lg p-2">
-            </div>
+            </div> --}}
 
             <div>
                 <label class="text-sm">Keterangan</label>
-                <input type="text" class="w-full border rounded-lg p-2">
+                <input type="text" class="w-full border rounded-lg p-2" wire:model="information">
+                @error('information')
+                        <span class="invalid-feedback">
+                                {{ $message }}
+                         </span>
+                @enderror
+            </div>
+
+
+            <!-- FOOTER -->
+            <div class="flex justify-end gap-3 mt-6">
+                <button
+                    x-on:click="$dispatch('close-modal', 'modal-tambah')"
+                    class="border px-4 py-2 rounded-lg">
+                    Batal
+                </button>
+
+                <button type="submit"
+                    class="bg-blue-600 text-white px-4 py-2 rounded-lg">
+                    Simpan
+                </button>
             </div>
 
         </form>
 
-        <!-- FOOTER -->
-        <div class="flex justify-end gap-3 mt-6">
-            <button
-                x-on:click="$dispatch('close-modal', 'modal-tambah')"
-                class="border px-4 py-2 rounded-lg">
-                Batal
-            </button>
-
-            <button
-                class="bg-blue-600 text-white px-4 py-2 rounded-lg">
-                Simpan (Dummy)
-            </button>
-        </div>
 
     </div>
 </x-modal>
