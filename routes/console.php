e958795e2c1de74787e87b49a58c1f2aef656279
FruHafizd <muhammadhafidz12333@gmail.com>
2026-01-08 23:39:55 +0800

    Fixing Generate Recurring

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('recurring:generate')->daily();
+Schedule::command('recurring:generate')->everyMinute();
